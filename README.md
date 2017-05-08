237bf4d66082fcc17408dbb32173dcaa3795383c
Nir Moav <nirm@wix.com>
2017-05-08 18:02:43 +0300

    First Commit - boot strapping - skeleton & ready to go!

---
+++ README.md
@@ -0,0 +1,9 @@
+# todo_mobx_tutorial - A simple Todo App using mobx &amp; React
+
+## Instructions on how to use
+1. Clone the project
+2. run "npm install" in the shell/command
+3. run "npm run start" in order to run webpack-dev-server and open the app in the browser
+
+
+*You can also run "npm run build" in order to run "webpack" as a one time thing, then you'll need to serve the application yourself.*
---
+++ webpack.config.js
@@ -0,0 +1,22 @@
+var path = require('path');
+
+module.exports = {
+  entry: './src/index.js',
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'dist')
+  },
+  module: {
+    loaders: [
+      {
+        test: /.jsx?$/,
+        loader: 'babel-loader',
+        exclude: /node_modules/,
+        query: {
+          presets: ['es2015', 'react', 'stage-1'],
+          plugins: ["transform-decorators-legacy"]
+        }
+      }
+    ]
+  }
+};
